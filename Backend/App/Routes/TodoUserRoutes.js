659578f59e5417e19a27444c67960cc022694326
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-08-28 23:11:45 -0500

    some change

--- Backend/App/Routes/TodoUserRoutes.js
+++ Backend/App/Routes/TodoUserRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import {totoresgister,todologin} from '../Controllers/TodoUserController.js'
-// import auth  from '../Middleware/Auth.js'
 const todouserRoute =  express.Router()
 todouserRoute.post('/register', totoresgister)
 todouserRoute.post('/login',todologin)
